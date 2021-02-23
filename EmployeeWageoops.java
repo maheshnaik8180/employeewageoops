f4d79d2c6e9e193c013a9bad1c54753e5488a09d
unknown <naikmahesh.naik@gmail.com>
2021-02-23 22:30:51 +0530

    update employeewage program employee is present or absent

--- EmployeeWageoops.java
+++ EmployeeWageoops.java
@@ -1,8 +1,20 @@
+import java.util.Random;
+
 public class EmployeeWageoops {
 
  public static void main(String[] args) {
 
 	System.out.println("Welcome to Employee wage computation program");
 
+	Random rnum = new Random();
+	int random = rnum.nextInt(2);
+	if(random==0)
+	{
+		System.out.println("Employee is Absent");
+	}
+	else
+	{
+		System.out.println("Employee is Present");
+}
 }
 }
